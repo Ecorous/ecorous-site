c806f9ccd5b6caf49ae2584862b6888cbfcdb80b
Exo <lennyj2468+la@gmail.com>
2022-06-18 21:53:23 +0100

    Add site

---
+++ src/components/Console.vue
@@ -0,0 +1,25 @@
+<script lang="js">
+export default {
+  data() {
+    return {
+      greeting: "Hello World!",
+      commands: ["help", "clear", "exit"],
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="console">
+    
+  </div>
+</template>
+
+<style>
+.console {
+  background-color: black;
+  border-radius: 4px;
+  padding: 20px;
+  margin-top: 90.665vh;
+}
+</style>
